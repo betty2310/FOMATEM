3dae828ab394622e691ca2591674582a8420827a
Huynh Dương <huynh.dh2310@gmail.com>
2022-06-24 16:24:50 +0700

    fix(file extension): csv

--- createDB.sql
+++ createDB.sql
@@ -173,11 +173,11 @@ CREATE TABLE training
 \COPY coach (coach_id, name, dob)  FROM 'coach.csv' WITH DELIMITER ',';
 \COPY club (club_id, coach_id, country, name) FROM 'club.csv' WITH DELIMITER ',';
 \COPY footballer (footballer_id, name, nationality, height, weight, dob, stronger_foot, club_id)  FROM 'footballer.csv' WITH DELIMITER ',';
-\COPY club_league (club_id, league) FROM 'club_league' WITH DELIMITER ',';
-\COPY club_trophy (club_id, name, date) FROM 'club_trophy' WITH DELIMITER ',';
-\COPY footballer_ability (footballer_id, last_update, offensive_awareness, ball_control, dribbling, tight_possession, low_pass, lofted_pass, finishing, heading, set_piece_taking, curl, speed, acceleration, kicking_power, jumping, physical_contact, balance, stamina, defensive_awareness, tackling, aggression, defensive_engagement, gk_awareness, gk_catching, gk_parrying, gk_reflexes, gk_reach, weak_foot_usage, weak_foot_accuracy, form, injury_resistance) FROM 'footballer_ability' WITH DELIMITER ',';
-\COPY footballer_position (footballer_id, position) FROM 'footballer_position' WITH DELIMITER ',';
-\COPY footballer_skill (footballer_id, skill) FROM 'footballer_skill' WITH DELIMITER ',';
+\COPY club_league (club_id, league) FROM 'club_league.csv' WITH DELIMITER ',';
+\COPY club_trophy (club_id, name, date) FROM 'club_trophy.csv' WITH DELIMITER ',';
+\COPY footballer_ability (footballer_id, last_update, offensive_awareness, ball_control, dribbling, tight_possession, low_pass, lofted_pass, finishing, heading, set_piece_taking, curl, speed, acceleration, kicking_power, jumping, physical_contact, balance, stamina, defensive_awareness, tackling, aggression, defensive_engagement, gk_awareness, gk_catching, gk_parrying, gk_reflexes, gk_reach, weak_foot_usage, weak_foot_accuracy, form, injury_resistance) FROM 'footballer_ability.csv' WITH DELIMITER ',';
+\COPY footballer_position (footballer_id, position) FROM 'footballer_position.csv' WITH DELIMITER ',';
+\COPY footballer_skill (footballer_id, skill) FROM 'footballer_skill.csv' WITH DELIMITER ',';
 \COPY enrollment (footballer_id, club_id, enrollment_start, enrollment_end, price, salary) FROM 'enrollment.csv' WITH DELIMITER ',';
 \COPY training (training_start, training_end) FROM 'training.csv' WITH DELIMITER ',';
 
