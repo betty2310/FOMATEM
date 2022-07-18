6092b9ac7fc2c5b2d16b22e08bceb312875a64ea
betty <huynh.dh2310@gmail.com>
2022-07-18 16:49:13 +0700

    fix: start up with login scene

--- App/src/main/java/com/betty2310/app/Main.java
+++ App/src/main/java/com/betty2310/app/Main.java
@@ -17,11 +17,13 @@ import java.io.IOException;
 public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        Parent root = FXMLLoader.load(getClass().getResource("LandingPage.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("Login.fxml"));
         Scene scene = new Scene(root);
-        scene.getStylesheets().add(getClass().getResource("style.css").toExternalForm());
+//        Parent root = FXMLLoader.load(getClass().getResource("LandingPage.fxml"));
+//        Scene scene = new Scene(root);
+//        scene.getStylesheets().add(getClass().getResource("style.css").toExternalForm());
         stage.setScene(scene);
-        stage.setTitle("FOMATEM");
+        stage.setTitle("Login to FOMATEM");
         stage.initStyle(StageStyle.UNIFIED);
         stage.getIcons().add(new Image(String.valueOf(getClass().getResource("icon/ball.png"))));
         stage.show();
