4f0b36236f5d02f5cf648c5626d5efc1aef7eeda
betty <huynh.dh2310@gmail.com>
2022-07-18 16:12:08 +0700

    feat(login): initial commit

---
+++ App/src/main/java/com/betty2310/app/Login.java
@@ -0,0 +1,9 @@
+package com.betty2310.app;
+
+import io.github.palexdev.materialfx.controls.MFXTextField;
+
+public class Login {
+    public MFXTextField database;
+    public MFXTextField user;
+    public MFXTextField pass;
+}
