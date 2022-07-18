4ad8e28c0e082b26a70e9e2a1e64977d71e193d8
betty <huynh.dh2310@gmail.com>
2022-07-18 16:48:56 +0700

    fix: change argument of database constant
    update with login input from user

--- App/src/main/java/com/betty2310/app/connection/Database.java
+++ App/src/main/java/com/betty2310/app/connection/Database.java
@@ -1,13 +1,16 @@
 package com.betty2310.app.connection;
 
+import com.betty2310.app.Login;
+
 import java.sql.*;
 
 public class Database {
-    private final String url = "jdbc:postgresql://localhost:5432/footballer_manager";
-    private final String user = "betty";
-    private final String pass = "";
-
     public Connection connection() {
+
+        final String url = "jdbc:postgresql://localhost:5432/" + Login.databaseName;
+        final String user = Login.userName;
+        final String pass = Login.passName;
+
         Connection conn = null;
         try {
             conn = DriverManager.getConnection(url, user, pass);
